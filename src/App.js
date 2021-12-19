ce603bd12699c2c60292dbbcd4e6d75bab166b3a
MartyVue <84153688+MartyVue@users.noreply.github.com>
2021-12-19 12:07:33 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+    <div className="container">
      <h1>Weather App</h1>
      <footer>
        This project was coded by Martha Vue and is open-sourced on{" "}
      <a href="https://github.com/MartyVue/weather-app-react"
        target="_blank">Github</a>
        </footer>
+       </div>
     </div>
   );
 }
