fec15706527c7c880ff49bf014791bbfa9b7010d
MartyVue <84153688+MartyVue@users.noreply.github.com>
2021-12-19 11:28:35 -0700

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
-     
+     <h1>Weather App</h1>
+     <footer>
+       This project was coded by Martha Vue and is open-sourced on{" "}
+     <a href="https://github.com/MartyVue/weather-app-react"
+       target="_blank">Github</a>
+       </footer>
     </div>
   );
 }
