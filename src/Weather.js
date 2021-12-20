576a28fff4e1ddd26f03e671afdbb5c7ea485a89
MartyVue <84153688+MartyVue@users.noreply.github.com>
2021-12-20 14:13:15 -0700

    edit temperature

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather() {
             <form>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a city" className="form-control" />
+                        <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on" />
                     </div>
                     <div className="col-3">
-                        <input type="submit" value="Search" className="btn btn-primary" />
+                        <input type="submit" value="Search" className="btn btn-primary w-100" />
                     </div>
                 </div>
             </form>
@@ -22,16 +22,17 @@ export default function Weather() {
             <div className="row">
                 <div className="col-6">
                     <img src="https://s.yimg.com/os/weather/1.0.1/shadow_icon/60x60/clear_day@2x.png" alt="Sunny"
-                    height="60px" widith="60px" ></img>
-                16°C
+                    height="60px" widith="60px" />
+                <span className="temperature">16</span>
+                <span className="unit">°C</span>
                 </div>
                 <div className="col-6">
                     <ul>
                         <h3>Details</h3>
-                        <li>Feels like 14°</li>
-                        <li>Humidity 9%</li>
-                        <li>Visibility 16.10 km</li>
-                        <li>UV Index 2 (Low)</li>
+                        <li className="weatherDetails">Feels like 14°</li>
+                        <li className="weatherDetails">Humidity 9%</li>
+                        <li className="weatherDetails">Visibility 16.10 km</li>
+                        <li className="weatherDetails">UV Index 2 (Low)</li>
                     </ul>
                 </div>
             </div>
