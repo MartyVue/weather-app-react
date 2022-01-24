677cb04d12d52484aa139b753eaf5705b06e0233
MartyVue <84153688+MartyVue@users.noreply.github.com>
2022-01-23 21:41:06 -0700

    add new component and unit to display relevant temp

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherIcon from "./WeatherIcon"
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -20,8 +21,7 @@ export default function WeatherInfo(props) {
                                 code={props.data.icon} 
                                 alt={props.data.description} 
                             />
-                    <span className="temperature">{Math.round(props.data.temperature)}</span>
-                    <span className="unit">°C</span>
+                            <WeatherTemperature celsius={props.data.temperature} />
                     </div>
                     </div>
                 </div>
