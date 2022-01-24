7380092cd56674e06b8ed0abc17fcac8796d979f
MartyVue <84153688+MartyVue@users.noreply.github.com>
2022-01-23 20:02:17 -0700

    corrected spelling on date to data

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,7 +17,7 @@ export default function WeatherInfo(props) {
                     <div className="clearfix">
                         <div className="float-left">
                             <WeatherIcon 
-                                code={props.date.icon} 
+                                code={props.data.icon} 
                                 alt={props.data.description} 
                             />
                         </div>
