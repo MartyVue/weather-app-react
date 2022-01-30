7984121cdbce68467b9cf8e4e938036c56e957c6
MartyVue <84153688+MartyVue@users.noreply.github.com>
2022-01-30 11:03:55 -0700

    added width for display to show more. added forecast js and css

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForcast from "./WeatherForcast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -62,7 +63,7 @@ export default function Weather(props) {
                 </div>
             </form>
             <WeatherInfo data={weatherData}/>
-           
+           <WeatherForcast />
         </div>
     );
     } else {
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -28,7 +28,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
     icon={codeMapping[props.code]}
     color="rgb(87, 157, 214)"
-    size={90}
+    size={props.size}
     animate={true}
   />
  );
