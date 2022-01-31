3ddd4eaad81692575cc9ea8eb20beb6dc0d29b3a
MartyVue <84153688+MartyVue@users.noreply.github.com>
2022-01-30 22:51:38 -0700

    edit main image to be larger

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -20,7 +20,7 @@ export default function WeatherInfo(props) {
                             <WeatherIcon 
                                 code={props.data.icon} 
                                 alt={props.data.description} 
-                                size={65}
+                                size={85}
                             />
                          </div>
 
